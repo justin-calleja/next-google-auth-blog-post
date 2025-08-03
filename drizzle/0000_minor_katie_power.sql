4f660cae16e7b2e9fc9bd6a1be0de98bc18499a9
Justin Calleja <justin.calleja@playnorth.com>
2025-08-03 21:36:13 +0200

    .

---
+++ drizzle/0000_minor_katie_power.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "tmp_session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"userId" serial NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "tmp_user" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"email" text,
+	CONSTRAINT "tmp_user_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "tmp_session" ADD CONSTRAINT "tmp_session_userId_tmp_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."tmp_user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "sessions_user_id_idx" ON "tmp_session" USING btree ("userId");
