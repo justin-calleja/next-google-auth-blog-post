1a0e34d4c24bef4aa1e02240486d54b96f0d2c66
Justin Calleja <justin.calleja@playnorth.com>
2025-08-09 09:04:02 +0200

    .

--- src/app/(auth)/api/sign-in/google/callback/route.ts
+++ src/app/(auth)/api/sign-in/google/callback/route.ts
@@ -1,21 +1,13 @@
 import { cookies } from "next/headers";
-import { OAuth2RequestError } from "arctic";
 import { googleAuth } from "@/lib/auth";
 import { db } from "@/db";
 import { eq } from "drizzle-orm";
 import { users } from "@/db/schema";
-
-// import { createGoogleUserUseCase } from "../../../../../../use-cases/users";
-// import { getAccountByGoogleIdUseCase } from "../../../../../../use-cases/accounts";
-// import { setSession } from "../../../../../../lib/session";
-// import { afterLoginUrl } from "../../../../../../app-config";
-// import { errorDict, NotRegisteredError } from "../../../../../../lib/errors";
+import { authPaths } from "@/lib/paths";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  console.log("🚀 ~ GET ~ url:", url);
   const code = url.searchParams.get("code");
-  console.log("🚀 ~ GET ~ code:", code);
   const state = url.searchParams.get("state");
   const allCookies = await cookies();
   const storedState = allCookies.get("google_oauth_state")?.value ?? null;
@@ -48,7 +40,6 @@ export async function GET(request: Request) {
     );
 
     const googleUser: GoogleUser = await response.json();
-    console.log("🚀 ~ GET ~ googleUser:", googleUser);
 
     const user = await db.query.users.findFirst({
       where: eq(users.email, googleUser.email),
@@ -56,7 +47,10 @@ export async function GET(request: Request) {
 
     if (!user) {
       return new Response(null, {
-        status: 400,
+        status: 302,
+        headers: {
+          Location: authPaths.unauthorizedUser,
+        },
       });
     }
     console.log("🚀 ~ GET ~ user:", user);
@@ -84,25 +78,13 @@ export async function GET(request: Request) {
     // });
   } catch (e) {
     console.error(e);
-    // // the specific error message depends on the provider
-    // if (e instanceof OAuth2RequestError) {
-    //   // invalid code
-    //   return new Response(null, {
-    //     status: 400,
-    //   });
-    // }
-    // if (e instanceof NotRegisteredError) {
-    //   // user is not registered/authorized to access this CMS
-    //   return new Response(null, {
-    //     status: 302,
-    //     headers: {
-    //       Location: errorDict.not_registered.redirectUrl,
-    //     },
-    //   });
-    // }
-    // return new Response(null, {
-    //   status: 500,
-    // });
+
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: authPaths.unauthorizedUser,
+      },
+    });
   }
 }
 
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { authPaths } from "@/lib/paths";
 
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Link href="/api/sign-in/google">
+      <Link href={authPaths.oauthStep1}>
         <Button className="cursor-pointer">Continue with Google</Button>
       </Link>
     </div>
