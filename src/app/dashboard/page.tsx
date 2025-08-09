7626aba0b803b680f987223e968cec701748e507
Justin Calleja <justin.calleja@playnorth.com>
2025-08-09 19:44:41 +0200

    .

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,5 +4,13 @@ import React from "react";
 export default async function Dashboard() {
   const user = await assertAuthenticated();
 
-  return <div>Hello {user.given_name}</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-2xl">
+          Hello <span className="font-bold">{user.given_name}</span>
+        </h1>
+      </div>
+    </div>
+  );
 }
